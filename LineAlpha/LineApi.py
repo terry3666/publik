6b39e3430268a09fa0be30f4b62e40dc16d16780
iamwijaya <31705906+iamwijaya@users.noreply.github.com>
2017-12-24 12:42:12 +0800

    Update LineApi.py

--- LineAlpha/LineApi.py
+++ LineAlpha/LineApi.py
@@ -65,6 +65,18 @@ class LINE:
   def updateSettings(self, settingObject):
     return self.Talk.client.updateSettings(0, settingObject)
 
+  def cloneContactProfile(self, mid):
+    contact = self.getContact(mid) 
+    profile = self.getProfile()
+    profile.displayName = contact.displayName
+    profile.statusMessage = contact.statusMessage
+    profile.pictureStatus = contact.pictureStatus
+    self.updateDisplayPicture(profile.pictureStatus)
+    return self.updateProfile(profile)
+    
+  def updateDisplayPicture(self, hash_id):
+    return self.Talk.client.updateProfileAttribute(0, 8, hash_id)
+
 
   """Operation"""
 
